4571d45d108dfc3947de7917690b7b34f058dde3
Doston <Doston>
2022-03-03 19:52:43 -0500

    budgecard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Button, Stack } from "react-bootstrap"
+import Container from "react-bootstrap/Container";
 
-import './App.css';
 
 function App() {
-  return " Hello !"
+  return <Container className="my-4"> 
+    <Stack direction="horizontal" gap="2" className="mb-4">
+      <h1 className='me-auto'> Budget </h1>
+      <Button variant="primary"> Add Budget </Button>
+      <Button variant="outline-primary"> Add Expense </Button>
+    </Stack>
+    <div style={{
+       display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", 
+        gap:"1rem", 
+        alignItems: "flex-start" 
+        }}></div>
+   </Container>
 }
 
 export default App;
