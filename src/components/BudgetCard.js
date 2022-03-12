dec835125fb584c6b0ff587e8bcc01a496bb4764
Doston <Doston>
2022-03-12 17:54:04 -0500

    now budget

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -31,10 +31,11 @@ export default function BudgetCard( {name, amount, max, gray } ) {
             variant={getProgressBarVariant(amount, max)}
             min={0}
             max={amount}
-            // now={amount}
+            now={amount}
             />
             <Stack direction="horizontal" gap="2" className='mt-4'>
               <Button variant="outline-primary" className='ms-auto'>Add Expense</Button>
+
               <Button variant="outline-secondary">View Expenses</Button>
             </Stack>
         </Card.Body>
