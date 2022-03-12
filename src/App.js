920903a7f4a93751ed230c40cb661633cde83b1d
Doston <Doston>
2022-03-11 19:20:11 -0500

    progress bar

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         gap:"1rem", 
         alignItems: "flex-start" 
         }}>
-          <BudgetCard name='Entertainment' amount={200} max={1000}></BudgetCard>
+          <BudgetCard name='Investment' amount={600} max={1000}></BudgetCard>
           </div>
    </Container>
 }
